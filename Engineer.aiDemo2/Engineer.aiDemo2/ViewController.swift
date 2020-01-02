29eac23ab04b31558793b0cf754dbb3259b5a269
Hiloni Sanghrajka <hiloni.sanghrajka@tatvasoft.com>
2020-01-02 17:52:44 +0530

    Initial Project setup

---
+++ Engineer.aiDemo2/Engineer.aiDemo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Engineer.aiDemo2
+//
+//  Created by PCQ177 on 02/01/20.
+//  Copyright © 2020 PCQ177. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Engineer.aiDemo2/Pods/Target Support Files/Pods-Engineer.aiDemo2/Pods-Engineer.aiDemo2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Engineer_aiDemo2 : NSObject
+@end
+@implementation PodsDummy_Pods_Engineer_aiDemo2
+@end
